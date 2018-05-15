07af414859c55ada45a3c4e32507d854d10758ea
DenisZaborov <ic17b019@technikum-wien.at>
2018-05-15 17:07:49 +0200

    add project Task2

---
+++ tasks/task2.1/task2.1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    public class Business
+
+    {
+        //instance variables
+        private string name;
+        private string address;
+
+        //properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        //contstructor
+        public Business(string name, string adress)
+        {
+            Name = name;
+            Address = adress;
+        }
+
+        //method
+        public void CheckLine()
+        {
+            int people = 0;
+            Console.Write("How many costumers are there?");
+            people = Convert.ToInt32(Console.ReadLine());
+
+            if (people >= 10)
+            {
+                Console.Write("There are too many customers in the line, open another register !");
+            }
+            if (people == 0)
+            {
+                Console.Write("There are no customer, you can leave the register !");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Business obj = new Business("Shopping Mall", "123 Street");
+            Console.WriteLine("{0}", obj.Name);
+            Console.WriteLine("{0}", obj.Address);
+            obj.CheckLine();
+        }
+    }
+}
