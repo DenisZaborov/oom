fed78b15e4c5c1f69288b1be6ddc3719afdf30c3
DenisZaborov <ic17b019@technikum-wien.at>
2018-06-26 17:34:08 +0200

    add project Task6

---
+++ Task6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace SubjectObserver
+{
+    class Program
+    {
+        static DataTrnsferHandler thandler; 
+        static GUIWindow form;                
+        static TransferDataNotification DataRecieved;  
+        static FileLogger logger;        
+
+        static void Main(string[] args)
+        {
+            startOUI();
+        }
+
+        public static void startOUI()
+        {
+            thandler = new DataTrnsferHandler();
+            thandler.InitializationComplete();
+            DataRecieved = new TransferDataNotification(thandler);
+            logger = new FileLogger(thandler);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GUIWindow(thandler));
+        }
+    }
+}
